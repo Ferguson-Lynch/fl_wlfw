90cc120fd0b5f58c8cd33ff71c2522394fbda78a
Alena <alenarote@gmail.com>
2024-06-13 17:56:27 -0400

    Pre-reorganizing the category/concern data structure

--- landowner-tool/src/components/ConcernPicker.vue
+++ landowner-tool/src/components/ConcernPicker.vue
@@ -5,12 +5,12 @@
   <div class="container">
     <form>
       <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Location</label>
-        <input type="email" class="form-control" id="exampleInputEmail1">
+        <label for="location" class="form-label">Location</label>
+        <input class="form-control" id="location">
       </div>
       <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Your Role</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
+        <label for="role" class="form-label">Your Role</label>
+        <input class="form-control" id="role">
       </div>
       <masonry :cols="3">
         <div v-for="category in categories" :key="category.category">
@@ -27,7 +27,7 @@
           </div>
         </div>
       </masonry>
-      <RouterLink to="/explore">
+      <RouterLink :to="'/explore?concerns=' + concernString">
         <button type="submit" class="btn btn-primary">Explore conservation practices</button>
       </RouterLink>
     </form>
